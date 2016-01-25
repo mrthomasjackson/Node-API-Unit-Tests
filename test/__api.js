40e2d488925cf26c13007cca95a664393dcc9428
mrthomasjackson <mrthomasjackson@icloud.com>
2016-01-25 16:35:10 -0500

    Resolved testing issue

--- test/__api.js
+++ test/__api.js
@@ -8,31 +8,31 @@ describe('API', function () {
   });
 
   afterEach(function(){
-    server.close;
+    server.close();
   });
 
-  it('"/api/" should load specified object.', function testHealth(done) {
-    request(server)
-    .get('/api/')
-    .set('Accept', 'application/json')
-    .expect('Content-Type', /json/)
-    .expect(200, {"hello": 'world'},done());
-  });
+  it('"/api" should load specified object.', function testHealth1(done) {
+        request(server)
+            .get('/api')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, {hello: 'world'}, done);
+    });
 
-  it('"/api/status" should return true.', function testHealth(done) {
+  it('"/api/status" should return true.', function testHealth2(done) {
     request(server)
     .get('/api/status')
     .set('Accept', 'application/json')
     .expect('Content-Type', /json/)
-    .expect(200, true, done());
+    .expect(200, {"healthy":"true"}, done);
   });
 
-  it('"/user/id" should return true.', function testHealth(done) {
+  it('"/user/id" should return 374.', function testHealth3(done) {
     var fakeUserID = 374;
     request(server)
     .get('/api/user/' + fakeUserID)
     .set('Accept', 'application/json')
     .expect('Content-Type', /json/)
-    .expect(200, {user: {id: fakeUserID}}, done());
+    .expect(200, {user: {id: fakeUserID}}, done);
   });
 });
